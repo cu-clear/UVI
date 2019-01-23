21d1390a7d74869b252c7f268aba03343a02c1e2
PrajnyaSatish <prajnya.12@gmail.com>
2019-01-23 13:51:20 -0700

    gitignore

--- uvi_web/.ipynb_checkpoints/forms-checkpoint.py
+++
@@ -1,8 +0,0 @@
-#uvi_web/forms.py
-
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class SomeForm(FlaskForm):
-	name = StringField('name_x', validators=[DataRequired()])
