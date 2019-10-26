510fdc0fbb386277fca8334751f27721169cf1a7
piyush-k-mishra <piyush.mishra@colorado.edu>
2019-10-26 17:48:45 -0600

    Initial commit: Changes to show copy-to-clipboard icon with verbnet classes and making the link available

---
+++ uvi_web/static/js/utility.js
@@ -0,0 +1,34 @@
+/*
+  Common utility file for entire uvi project
+  Format of writing:
+    * Add the name of file or feature (in case of multiple files) the function is being used in
+    * Mention the reference (Ref) if the code is being reused for an external source
+*/
+
+/**
+ * 
+ * @param {object} ctx 
+ * @param {string} className 
+ * 
+ * Function to copy path of the verbnet class/subclass.
+ * Usage in: files with name render_verbnet(_*).html
+ */
+function copyToClipboard(ctx, className) {
+    var classPath = new URL(window.location.origin + className);
+    // Ref: https://techoverflow.net/2018/03/30/copying-strings-to-the-clipboard-using-pure-javascript/
+    var el = document.createElement('textarea');
+    // Set value (string to be copied)
+    el.value = classPath.href;
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '');
+    el.style = {position: 'absolute', left: '-9999px'};
+    document.body.appendChild(el);
+    // Select text inside element
+    el.select();
+    // Copy text to clipboard
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'Copied!' : 'Whoops, not copied!';
+    $(ctx).find('i').attr('data-original-title', msg).tooltip('show');
+    // Remove temporary element
+    document.body.removeChild(el);
+}
