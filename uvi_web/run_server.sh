c926f95c6c6ecde497192926fd7c3252b7c67a2b
Piyush Mishra <54338352+piyush-k-mishra@users.noreply.github.com>
2020-09-09 11:36:10 -0600

    Modified shell script for restarting server
    - Added service command to stop and start the server.
    - Earlier exec command is commented so that the new one can work properly.

--- uvi_web/run_server.sh
+++ uvi_web/run_server.sh
@@ -2,7 +2,9 @@
 export FLASK_APP=uvi_flask.py
 export FLASK_DEBUG=0
 
+sudo service gunicorn_uvi stop
 nohup python -u monitor_corpora.py &
 echo $! >> "monitor_script_PID"
 
-exec gunicorn -w 4 -b 127.0.0.1:4000 uvi_flask:app
+#exec gunicorn -w 4 -b 127.0.0.1:4000 uvi_flask:app
+sudo service gunicorn_uvi start
