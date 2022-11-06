0fcce0e598c712d371895558fe40f5411e9a438d
S.S.Akhilesh Appala <14235798+akhileshappala@users.noreply.github.com>
2022-11-06 08:02:37 -0700

    reversing the changes for dev branch as it wont work in server as installations are not there

--- uvi_web/build_mongo_collections.py
+++ uvi_web/build_mongo_collections.py
@@ -331,7 +331,7 @@ def build_verbnet_collection():
 	#VERBNET
 	import spacy
 	from spacy import displacy
-	spacy_nlp = spacy.load('en_core_web_sm')
+	spacy_nlp = spacy.load('en')
 
 	with open(path_fd) as rf:
 		fd_list =  json.load(rf)
